cc259a6efeb4ac4ab9549ba108ef4f6865d2e1fd
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-06-03 13:38:59 -0700

    ChannelVideo implemented

--- youtube/src/SearchPage.js
+++ youtube/src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./SearchPage.css";
 import TuneIcon from "@material-ui/icons/Tune";
+import { ChannelRow } from "./ChannelRow";
 
 export const SearchPage = () => {
   return (
@@ -9,6 +10,16 @@ export const SearchPage = () => {
         <TuneIcon />
         <h2>Filter</h2>
       </div>
+      <hr />
+
+      <ChannelRow
+        image="https://yt3.ggpht.com/a/AATXAJyZBQmbGZE0RH4Rzh0my2iSArzhfVgUXY5iU8c1NYA=s900-c-k-c0x00ffffff-no-rj"
+        channel="T-Series"
+        verified
+        subs="160M"
+        noOfVideos={(14, 780)}
+        description="Music can change the world. T-Series is India's largest Music Label & Movie Studio, believes in bringing world close together through its music."
+      />
 
       <hr />
     </div>
